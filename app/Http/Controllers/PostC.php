d28694763b1cbe8eda17988e52e4e2f3e6757347
dianwinata09 <msocialonly@gmail.com>
2023-03-01 15:03:38 +0700

    index crud

---
+++ app/Http/Controllers/PostC.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PostM;
+use Illuminate\Http\Request;
+use App\Http\Resources\PostR;
+
+class PostC extends Controller
+{
+    public function index(){
+        $posts = PostM::latest()->paginate(5);
+
+        return new PostR(true, 'List Data Posts', $posts);
+    }
+}
